5ea009d1aeed0465cf5792bbc143f25698f5cec5
voynow <voynow99@gmail.com>
2022-11-29 08:45:52 -0500

    Add functions to utils file, update viz

--- strava_utils.py
+++ strava_utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 bucket_name = "strava-raw"
 s3 = boto3.resource('s3')
 
+
 def get_activities():
     """
     Get activities JSON from raw bucket
@@ -15,6 +16,18 @@ def get_activities():
         body_bytes = obj.get()['Body'].read()
         activities.append(json.loads(body_bytes))
     return activities
+
+
+def clean(df):
+    
+    # update date col as pandas datetime
+    df['date'] = pd.to_datetime(df['start_date'].apply(lambda x: x.split("T")[0]))
+    df.drop(['start_date'], axis=1, inplace=True)
+    
+    # meters -> miles
+    df['distance'] *= 0.000621371
+    
+    return df
     
 
 def activities_to_df(activities):
@@ -33,5 +46,20 @@ def activities_to_df(activities):
                 remove_keys.append(key)
         for key in remove_keys:
             unique_keys.remove(key)
+    
+    activities = [{key: activity[key] for key in unique_keys} for activity in activities]
+    return clean(pd.DataFrame(activities))
 
-    return pd.DataFrame([{key: activity[key] for key in unique_keys} for activity in activities])
+
+def fill_missing_dates(df):
+    """
+    Add rows for dates where no activity exists
+    """
+    if 'date' not in df.columns:
+        raise ValueError("Missing required column: 'date'")
+    
+    date_range = pd.date_range(df['date'].min(), df['date'].max())
+    dates_df = pd.DataFrame(date_range, columns=['date'])
+    joined_df = dates_df.set_index('date').join(df.set_index('date'))
+    
+    return joined_df
